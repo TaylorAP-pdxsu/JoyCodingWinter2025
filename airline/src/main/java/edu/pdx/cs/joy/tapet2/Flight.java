04c8fa7827f0645ab917db054fd64ae9884b8e55
tapet2 <tapet2@pdx.edu>
2025-01-27 16:29:07 -0800

    implemented flight & airline get functions

--- airline/src/main/java/edu/pdx/cs/joy/tapet2/Flight.java
+++ airline/src/main/java/edu/pdx/cs/joy/tapet2/Flight.java
@@ -3,6 +3,25 @@ package edu.pdx.cs.joy.tapet2;
 import edu.pdx.cs.joy.AbstractFlight;
 
 public class Flight extends AbstractFlight {
+  private int flightNum;
+  private String sourceAirport;
+  private String departTime;
+  private String destAirport;
+  private String arrivalTime;
+  
+  public Flight() {}
+
+  public Flight(int flightNum,
+                String sourceAirport, String departTime,
+                String destAirport, String arrivalTime)
+  {
+    this.flightNum = flightNum;
+    this.sourceAirport = sourceAirport;
+    this.departTime = departTime;
+    this.destAirport = destAirport;
+    this.arrivalTime = arrivalTime;
+  }
+
   @Override
   public int getNumber() {
     return 42;
@@ -10,19 +29,21 @@ public class Flight extends AbstractFlight {
 
   @Override
   public String getSource() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return sourceAirport;
   }
 
+  //ignore
   @Override
   public String getDepartureString() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return departTime;
   }
 
   @Override
   public String getDestination() {
-    throw new UnsupportedOperationException("This method is not implemented yet");
+    return destAirport;
   }
 
+  //ignore
   @Override
   public String getArrivalString() {
     throw new UnsupportedOperationException("This method is not implemented yet");
