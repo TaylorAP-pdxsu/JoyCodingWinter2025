d1e66fd2d6ece9d85c16fd891f6676c23848c5a5
Taylor Pettingill <134963416+Kungen13@users.noreply.github.com>
2025-01-30 20:46:52 -0800

    parser revision

--- airline/src/main/java/edu/pdx/cs/joy/tapet2/Project1.java
+++ airline/src/main/java/edu/pdx/cs/joy/tapet2/Project1.java
@@ -12,57 +12,86 @@ public class Project1 {
     return true;
   }
 
-  static boolean isValidArgs(String[] args) {
-    if(args.length == 0)
+  static int isValidArgs(String[] args) {
+    //no args given
+    int lengthOfArgs = args.length;
+    if(lengthOfArgs == 0)
     {
-      System.err.println("Missing command line arguments");
-      return false;
+      throw new IllegalArgumentException("ERROR: Missing command line arguments...");
     }
-    if(args.length > 8)
+
+    //validate given args
+    int startOfInfo = 0;
+    int i;
+    for(i = 0; args[i].charAt(0) == '-' && i < lengthOfArgs-1; ++i)
     {
-      System.err.println("Incorrect number of arguments given...");
-      return false;
+      if(!args[i].equals("-print") && !args[i].equals("-README"))
+      {
+        System.err.println("\nERROR: Unknown command line option..."
+                          + "\nValid options include: -print and -README");
+        throw new IllegalArgumentException("ERROR occured at argument " + (startOfInfo+i) + " with value => " + args[i]);
+      }
     }
-    if(!args[2].matches("\\d+"))
+
+    startOfInfo = i;
+
+    if(args.length > startOfInfo + 8)
     {
-      System.err.println("ERROR: Flight number is not an integer...");
-      return false;
+      throw new IllegalArgumentException("ERROR: Incorrect number of arguments given...");
     }
-    if(args[3].length() > 3 || args[3].matches("[a-zA-Z]"))
+    if(!args[startOfInfo+1].matches("\\d+"))
     {
-      System.err.println("Departure Airport Code is too long or contains number, please enter 3-letter code...");
-      return false;
+      System.err.println("\nERROR: Flight number is not an integer...");
+      throw new IllegalArgumentException("ERROR occured at argument " + (startOfInfo+1)
+                                        + " with value => " + args[startOfInfo+1]);
     }
-    if(args[5].length() > 3 || args[5].matches("a-zA-Z"))
+    if(args[startOfInfo+2].length() > 3 || args[startOfInfo+2].matches("[a-zA-Z]"))
     {
-      System.err.println("Arrival Airport Code is too long or contains number, please enter 3-letter code...");
-      return false;
+      System.err.println("\nERROR: Departure Airport Code is too long or contains number, please enter 3-letter code...");
+      throw new IllegalArgumentException("ERROR occured at argument " + (startOfInfo+2)
+                                        + " with value => " + args[startOfInfo+2]);
+    }
+    if(args[startOfInfo+5].length() > 3 || args[startOfInfo+5].matches("a-zA-Z"))
+    {
+      System.err.println("\nERROR: Arrival Airport Code is too long or contains number, please enter 3-letter code...");
+      throw new IllegalArgumentException("ERROR occured at argument " + (startOfInfo+5)
+                                        + " with value => " + args[startOfInfo+5]);
     }
     //create validation for date/time in project 2
-    return true;
+    return startOfInfo;
   }
 
   public static void main(String[] args) {
-    if(!isValidArgs(args))
-    {
+    int startOfInfo;
+    try {
+      startOfInfo = isValidArgs(args);
+    } catch (IllegalArgumentException e) {
+      System.err.println(e);
       System.err.println("\nPlease enter airline and flight information in format:"
                         + "\n(AIRLINE NAME) (FLIGHT NUMBER) (DEPARTURE AIRPORT CODE)"
                         + " (DEPARTURE DATE & TIME) (ARRIVAL AIRPORT CODE) (ARRIVAL DATE & TIME)"
                         + "\n\nDate should be formatted as MM/DD/YYYY"
                         + "\nTime should be formatted as HR:MM");
+      return;
     }
-    else
-    {
-      Flight flight = new Flight(Integer.parseInt(args[2]), args[3], args[4], args[5], args[6]);  // Refer to one of Dave's classes so that we can be sure it is on the classpath
-      
-      Airline airline = new Airline(args[0]);
-      //Will change Airline to have more proper input methodology in future
-      airline.getFlights().add(flight);
+    Airline airline = new Airline(args[startOfInfo]);
+    Flight flight = new Flight(Integer.parseInt(args[startOfInfo+1]),
+                                args[startOfInfo+2],
+                                args[startOfInfo+3] + args[startOfInfo+4],
+                                args[startOfInfo+5],
+                                args[startOfInfo+6] + args[startOfInfo+7]);  // Refer to one of Dave's classes so that we can be sure it is on the classpath
+    
+    //Will change Airline to have more proper input methodology in future
+    airline.getFlights().add(flight);
+    if(args[0].equals("-print") || args[1].equals("-print"))
       System.out.println(flight.toString());
+    
+    if(args[0].equals("-README") || args[1].equals("-README"))
+    {
+      //do readme stuff
     }
     /*for (String arg : args) {
       System.out.println(arg);
     }*/
   }
-
 }
