3e4f5b1bea639b8135013a636c91e9dc753e61a9
tapet2 <tapet2@pdx.edu>
2025-03-16 11:09:15 -0700

    Set up UI and previous project files

--- android-airline/app/src/main/java/edu/pdx/cs/joy/tapet2/FlightActivity.java
+++ android-airline/app/src/main/java/edu/pdx/cs/joy/tapet2/FlightActivity.java
@@ -10,13 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class FlightActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_flight);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -24,8 +24,12 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void LaunchApp(View view) {
-        Intent intent = new Intent(this, FlightActivity.class);
+    public void GoToSearchPage(View view) {
+        Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
+    }
+
+    public void AddFlight_OnButtonClick(View view) {
+
     }
 }
